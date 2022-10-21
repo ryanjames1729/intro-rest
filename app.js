8a8d8251555f9c97a2125cf576fdf7c4fc704cc5
Ryan James <rjames@rjames-mbpr-it.lan>
2022-10-21 17:58:49 -0400

    heroku setup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // Set port number
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
